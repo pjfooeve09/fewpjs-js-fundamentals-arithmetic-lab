c5a99596d13b3407f10bccf17b3c954c885e1395
Parham Javadi <pjfooeve09@yahoo.com>
2021-06-28 11:15:04 -0700

    commit tree 762909e4a8f2db5e0fb5fc7342ec22ead114e543

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+
+let num3 = 244
+let num4 = 15
+let mod = num3 % num4
+
+const max = Math.max(10, 20)
